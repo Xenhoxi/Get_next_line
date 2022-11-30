28cc84674d799b7497475aec29f5d74ffb4d9dd1
Leon Jerineck <ljerinec@k0r4p4.42mulhouse.fr>
2022-11-30 18:36:11 +0100

    GNL2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 23:44:34 by ljerinec          #+#    #+#             */
-/*   Updated: 2022/11/30 03:25:49 by ljerinec         ###   ########.fr       */
+/*   Updated: 2022/11/30 18:13:28 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,11 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 		i++;
 	return (i);
-}
-
-char	*all_before_backslash_n(char *save)
-{
-	int		i;
-	//char	*result;
-
-	i = 0;
-	while (save[i] != '\x03' || save[i] != '\0')
-	{
-		printf("%c", save[i]);
-		i++;
-	}
-	//result = malloc(sizeof(char) * (i + 1));
-	//if (!result)
-	//	return (NULL);
-	// i = 0;
-	// while (save[i] != '\n' || save[i] != '\0')
-	// {
-	// 	result[i] = save[i];
-	// 	i++;
-	// }
-	// result[i] = save[i];
-	// result[i] = 0;
-	return (NULL);
 }
 
 int	check_backslash(char *save)
@@ -63,54 +40,45 @@ int	check_backslash(char *save)
 	return (0);
 }
 
-char	*ft_strjoin(char *str, char *buffer)
+char	*ft_strjoin(char *str, char *buffer, int len)
 {
 	int		u;
 	int		i;
-	int		lenmalloc;
 	char	*save;
 
 	i = -1;
 	u = 0;
-	lenmalloc = ft_strlen(str) + ft_strlen(buffer) + 1;
-	save = malloc(sizeof(char) * lenmalloc);
+	save = malloc(sizeof(char) * len);
 	if (!save)
 		return (NULL);
-	while (str[++i])
-		save[u++] = str[i];
+	if (str)
+		while (str[++i])
+			save[u++] = str[i];
 	i = -1;
 	while (buffer[++i])
 		save[u++] = buffer[i];
-	save[u] = '\0';
 	free(str);
 	return (save);
 }
 
 char	*get_next_line(int fd)
 {
-	char	buffer[BUFFER_SIZE];
-	int		i;
-	char	*save;
-	int		end;
-	char	*result;
-	char	*test;
+	char		buffer[BUFFER_SIZE];
+	int			i;
+	static char	*save;
+	int			end;
 
 	i = 0;
 	end = 0;
-	save = malloc(sizeof(char));
-	if (!save)
-		return (NULL);
-	save[0] = '\0';
+	save = NULL;
 	while (!end)
 	{
 		i += read(fd, buffer, BUFFER_SIZE);
-		test = ft_strjoin(save, buffer);
+		save = ft_strjoin(save, buffer, i + 1);
 		if (check_backslash(save))
 			end = 1;
 	}
-	printf("%s", save);
-	result = all_before_backslash_n(test);
-	return (NULL);
+	return (save);
 }
 
 int	main(void)
