ebe7f8f4ab731eda09127f4e73749a5f24477423
Leon Jerineck <ljerinec@k1r4p4.42mulhouse.fr>
2022-12-01 01:31:46 +0100

    GNL3

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 23:44:34 by ljerinec          #+#    #+#             */
-/*   Updated: 2022/11/30 18:13:28 by ljerinec         ###   ########.fr       */
+/*   Updated: 2022/12/01 01:31:09 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ int	check_backslash(char *save)
 	int	i;
 
 	i = -1;
+	if (!save)
+		return (0);
 	while (save[++i])
 	{
 		if (save[i] == '\0' || save[i] == '\n')
@@ -40,23 +42,83 @@ int	check_backslash(char *save)
 	return (0);
 }
 
+char	*all_before_backslash_n(char *str)
+{
+	int		i;
+	int		u;
+	char	*result;
+
+	i = 0;
+	u = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	result = malloc(sizeof(char) * (i + 1));
+	if (!result)
+		return (NULL);
+	u = 0;
+	while (u <= i)
+	{
+		result[u] = str[u];
+		u++;
+	}
+	result[u] = str[u];
+	return (result);
+}
+
+char	*all_after_backslash_n(char *str)
+{
+	int		i;
+	int		u;
+	char	*result;
+	int		len_str;
+
+	i = 0;
+	u = 0;
+	result = NULL;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	len_str = ft_strlen(str) -1 - i;
+	u = 0;
+	i++;
+	if (len_str != 0)
+		result = malloc(sizeof(char) * len_str);
+	if (!result || len_str == 0)
+		return (NULL);
+	while (u <= len_str)
+	{
+		result[u] = str[i];
+		u++;
+		i++;
+	}
+	return (result);
+}
+
 char	*ft_strjoin(char *str, char *buffer, int len)
 {
 	int		u;
 	int		i;
 	char	*save;
 
-	i = -1;
+	i = 0;
 	u = 0;
-	save = malloc(sizeof(char) * len);
-	if (!save)
-		return (NULL);
-	if (str)
-		while (str[++i])
-			save[u++] = str[i];
-	i = -1;
-	while (buffer[++i])
-		save[u++] = buffer[i];
+	save = malloc(sizeof(char) * (len + ft_strlen(str) + 1));
+	if (str != NULL)
+	{
+		while (str[i] != '\n' && str[i] != '\0')
+		{
+			save[u] = str[i];
+			i++;
+			u++;
+		}
+	}
+	i = 0;
+	while (u < len)
+	{
+		save[u] = buffer[i];
+		u++;
+		i++;
+	}
+	save[u] = 0;
 	free(str);
 	return (save);
 }
@@ -67,18 +129,25 @@ char	*get_next_line(int fd)
 	int			i;
 	static char	*save;
 	int			end;
+	char		*result;
+	int			u;
 
 	i = 0;
 	end = 0;
-	save = NULL;
-	while (!end)
+	u = 1;
+	if (check_backslash(save))
+			end = 1;
+	while (!end && u)
 	{
-		i += read(fd, buffer, BUFFER_SIZE);
-		save = ft_strjoin(save, buffer, i + 1);
+		u = read(fd, buffer, BUFFER_SIZE);
+		i += u;
+		save = ft_strjoin(save, buffer, i);
 		if (check_backslash(save))
 			end = 1;
 	}
-	return (save);
+	result = all_before_backslash_n(save);
+	save = all_after_backslash_n(save);
+	return (result);
 }
 
 int	main(void)
@@ -87,6 +156,22 @@ int	main(void)
 	char	*result;
 
 	fd = open("read.txt", O_RDONLY);
+	result = get_next_line(fd);
+	printf("%s", result);
+	result = get_next_line(fd);
+	printf("%s", result);
+	result = get_next_line(fd);
+	printf("%s", result);
+	result = get_next_line(fd);
+	printf("%s", result);
+	result = get_next_line(fd);
+	printf("%s", result);
+	result = get_next_line(fd);
+	printf("%s", result);
+	result = get_next_line(fd);
+	printf("%s", result);
+	result = get_next_line(fd);
+	printf("%s", result);
 	result = get_next_line(fd);
 	printf("%s", result);
 	return (0);
